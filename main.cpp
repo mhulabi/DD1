7b40be33451de0fe9caf1e2c3993616ccc4577f7
andrewsinout <70208780+andrewsinout@users.noreply.github.com>
2020-10-31 15:06:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -98,6 +98,7 @@ bool found (int x, string y){
     return false;
 }
 
+
 vector<pair<string,string>> comparison (int v, vector<pair<string,string>> a, vector<pair<string,string>> b, vector<bool>& check_a,vector<bool>& check_b){
     vector<pair<string,string>> group,changed;
     string one,two;
@@ -250,7 +251,7 @@ int main(){
                     }
                 }
                 for(int y=0;y<check_c.size();y++){
-                    if((check_c[y]==true) && (y % 2 == 0)){
+                    if((check_c[y]==true)){
                         PI.push_back(itterations[n][i][y]);
                     }
                 }
@@ -260,7 +261,7 @@ int main(){
                 check_a.clear();
             }
             for(int y=0;y<check_c.size();y++){
-                if((check_c[y]==true) && (y % 2 == 0)){
+                if((check_c[y]==true)){
                     PI.push_back(itterations[n][var][y]);
                 }
             }
@@ -275,26 +276,22 @@ int main(){
                 }
         }
         
-        
-        
         cout<<"----------------------------------------"<<endl;
-        cout<<endl<<"The prime implicatns :"<<endl<<endl;;
-        for (int i=0;i<PI.size();i++) {
+        cout<<endl<<"The prime implicatns :"<<endl;
+        for (int i = 0;i<PI.size();i++) {
             cout<<PI[i].first<<"("<<PI[i].second<<")"<<endl;
         }
         
-        cout<<"----------------------------"<<endl;
-        cout<<endl<<"The boolean equation for the prime implicatns :"<<endl;
-        string L="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        for(int j=0;j<PI.size();j++){
-            for(int i=0;i<v;i++){
-                if(PI[j].first[i]=='0')
-                    cout<<L[i]<<"'";
-                if(PI[j].first[i]=='1')
-                    cout<<L[i];
+        
+        vector<pair<string,string>> PII;
+        for(int i=0;i<PI.size();i++)
+            if(PI[i].first!=PI[i+1].first){
+                PII.push_back(PI[i]);
             }
-            if(j!=PI.size()-1)
-                cout<<"+";
+        
+        cout<<endl<<"The prime implicatns :"<<endl;
+        for (int i = 0;i<PII.size();i++) {
+            cout<<PII[i].first<<"("<<PII[i].second<<")"<<endl;
         }
         
         cout<<endl<<endl<<"----------------------------"<<endl;
@@ -302,43 +299,28 @@ int main(){
                 vector<pair<string, string>> EPI;
                 vector <int> index;
 
-                for (int i = 0 ; i < m.size(); i++){
-                    int counter = 0;
-                    for (int j = 0; j < PI.size(); j++){
-                        if (found(m[i], PI[j].second) == true){
+                for (int i=0;i<m.size();i++){
+                    int counter=0;
+                    for (int j=0;j<PII.size();j++){
+                        if (found(m[i],PII[j].second)==true){
                             counter++;
                             index.push_back(j);
                         }
                     }
-                    if (counter == 1){
+                    if (counter==1){
                         
-                        EPI.push_back(PI[index[0]]);
+                        EPI.push_back(PII[index[0]]);
                     }
                     index.clear();
                 }
-                for (int i = 0;i<EPI.size();i++) {
+                for (int i=0;i<EPI.size();i++) {
                     cout<<EPI[i].first<<"("<<EPI[i].second<<")"<<endl;
                 }
+
         //insert the rest of the main here
-        
-        cout<<"----------------------------"<<endl;
-        cout<<endl<<"The boolean equation for the Essential prime implicatns :"<<endl;
-        for(int j=0;j<EPI.size();j++){
-            for(int i=0;i<v;i++){
-                if(EPI[j].first[i]=='0')
-                    cout<<L[i]<<"'";
-                if(EPI[j].first[i]=='1')
-                    cout<<L[i];
-            }
-            if(j!=EPI.size()-1)
-                cout<<"+";
-        }
-        
-        cout<<endl<<endl;
     }
     else{
     cout << "Data in file is not correct according to the number of variables present please change the data in the file and try again"<< endl;
     }
     return 0;
 }
-
